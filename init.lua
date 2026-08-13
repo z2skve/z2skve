f37df6d6cf4fc788b9b5595ce32822bd527d15f9
t8rx <z2skve@gmail.com>
2026-08-13 03:15:39 +0200

    fixed tab lenght on clangd

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.ignorecase = true
@@ -37,12 +40,128 @@ vim.g.maplocalleader = "\\"
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
+        {
+      "nvim-neo-tree/neo-tree.nvim",
+      dependencies = {
+        {"nvim-lua/plenary.nvim"},
+        {"MunifTanjim/nui.nvim"},
+        {"nvim-tree/nvim-web-devicons"}
+      },
+      cmd = "Neotree",
+      keys = {
+        {
+          "<leader>fe",
+          function()
+            require("neo-tree.command").execute({ toggle = true, dir = vim.fn.getcwd() })
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+        },
+        {
+          "<leader>fE",
+          function()
+            require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+          end,
+          desc = "Explorer NeoTree (cwd)",
+        },
+        { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+        { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+        {
+          "<leader>ge",
+          function()
+            require("neo-tree.command").execute({ source = "git_status", toggle = true })
+          end,
+          desc = "Git Explorer",
+        },
+        {
+          "<leader>be",
+          function()
+            require("neo-tree.command").execute({ source = "buffers", toggle = true })
+          end,
+          desc = "Buffer Explorer",
+        },
+      },
+      deactivate = function()
+        vim.cmd([[Neotree close]])
+      end,
+      init = function()
+        vim.api.nvim_create_autocmd("BufEnter", {
+          group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
+          desc = "Start Neo-tree with directory",
+          once = true,
+          callback = function()
+            if package.loaded["neo-tree"] then
+              return
+            else
+              local stats = vim.uv.fs_stat(vim.fn.argv(0)) 
+              if stats and stats.type == "directory" then
+                require("neo-tree")
+              end
+            end
+          end,
+        })
+      end,
+      opts = {
+        sources = { "filesystem", "buffers", "git_status" },
+        open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+        filesystem = {
+          bind_to_cwd = false,
+          follow_current_file = { enabled = true },
+          use_libuv_file_watcher = true,
+        },
+        window = {
+          mappings = {
+            ["l"] = "open",
+            ["h"] = "close_node",
+            ["<space>"] = "none",
+            ["Y"] = {
+              function(state)
+                local node = state.tree:get_node()
+                local path = node:get_id()
+                vim.fn.setreg("+", path, "c")
+              end,
+              desc = "Copy Path to Clipboard",
+            },
+            ["O"] = {
+              function(state)
+                require("lazy.util").open(state.tree:get_node().path, { system = true })
+              end,
+              desc = "Open with System Application",
+            },
+            ["P"] = { "toggle_preview", config = { use_float = false } },
+          },
+        },
+        default_component_configs = {
+          indent = {
+            with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+            expander_collapsed = "",
+            expander_expanded = "",
+            expander_highlight = "NeoTreeExpander",
+          },
+          git_status = {
+            symbols = {
+              unstaged = "󰄱",
+              staged = "󰱒",
+            },
+          },
+        },
+      },
+      config = function(_, opts)
+        require("neo-tree").setup(opts)
+        vim.api.nvim_create_autocmd("TermClose", {
+          pattern = "*lazygit",
+          callback = function()
+            if package.loaded["neo-tree.sources.git_status"] then
+              require("neo-tree.sources.git_status").refresh()
+            end
+          end,
+        })
+      end,
+    },
+    {"neovim/nvim-lspconfig"},
     {
         "mason-org/mason-lspconfig.nvim",
-        opts = {},
         dependencies = {
-            { "mason-org/mason.nvim", opts = {} },
-            "neovim/nvim-lspconfig",
+        	{ "mason-org/mason.nvim", opts = {} },
         },
     },
    {
@@ -61,7 +180,6 @@ require("lazy").setup({
         local cmp = require("cmp")
         local defaults = require("cmp.config.default")()
         local auto_select = true
-        
         return {
           auto_brackets = {}, -- configure any filetype to auto add brackets
           completion = {
@@ -148,15 +266,39 @@ require("lazy").setup({
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
-  
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
 
+require("mason").setup()
+
+vim.lsp.config("clangd", {
+    cmd = {
+        "clangd",
+        "--background-index",
+        "--fallback-style=WebKit",
+    },
+})
+
+require("mason-lspconfig").setup {
+    ensure_installed = { "clangd"},
+    automatic_enable = true
+}
+
+vim.keymap.set("n", "<leader>f", function()
+    vim.lsp.buf.format({
+        async = true,
+        formatting_options = {
+            tabSize = 4,
+            insertSpaces = true,
+        },
+    })
+end, { desc = "Formatear documento con LSP (4 espacios)" })
+
 -- Diagnostics
 vim.diagnostic.config({
     virtual_text = {
-        prefix = '●', 
+        prefix = '●',
         spacing = 4,
     },
     signs = true,
